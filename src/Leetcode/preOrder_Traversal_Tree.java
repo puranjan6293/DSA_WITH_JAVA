38817ebea1f66431e99e3f3f5d70008aaffbaf25
puranjan <mallik.puranjan@gmail.com>
2022-10-26 22:29:43 +0530

    pre order traversal

---
+++ src/Leetcode/preOrder_Traversal_Tree.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class preOrder_Traversal_Tree {
+    /*
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        preOrder(root, ans);
+        return ans;
+    }
+    static void preOrder(TreeNode root, List<Integer> lis){
+        if(root==null) return;
+        lis.add(root.val);
+        preOrder(root.left, lis);
+        preOrder(root.right, lis);
+    }
+     */
+}
