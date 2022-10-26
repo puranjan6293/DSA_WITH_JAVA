fcdef348ad41e42f5b257c55c252e7e1f09d65ec
puranjan <mallik.puranjan@gmail.com>
2022-10-26 17:23:50 +0530

    is sametree

---
+++ src/Leetcode/Is_SameTree.java
@@ -0,0 +1,15 @@
+package Leetcode;
+
+public class Is_SameTree {
+    /*
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        
+        if(p==null || q==null || p.val!=q.val) return false;
+        
+        //traverse in both left and both right
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+     */
+    
+}
