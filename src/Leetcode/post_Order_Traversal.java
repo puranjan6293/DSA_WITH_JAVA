f679884f4d8c4310ff186e0ac40f75a7fb4633a7
puranjan <mallik.puranjan@gmail.com>
2022-10-26 22:34:18 +0530

    post order traversal

---
+++ src/Leetcode/post_Order_Traversal.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class post_Order_Traversal {
+    /*
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        postOrder(root, ans);
+        return ans;
+    }
+    static void postOrder(TreeNode root, List<Integer> lis){
+        if(root==null) return;
+        postOrder(root.left, lis);
+        postOrder(root.right, lis);
+        lis.add(root.val);
+    }
+     */
+}
