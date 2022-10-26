f051943f1c7771f0e21f41b2d4cf116e69be4e02
puranjan <mallik.puranjan@gmail.com>
2022-10-26 22:18:50 +0530

    path sum

---
+++ src/Leetcode/path_Sum.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class path_Sum {
+    /* 
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        //Approach1
+        //base case
+        if(root==null){
+            return false;
+        }
+        
+        if(root.left==null && root.right==null && targetSum==root.val){
+            return true;
+        }
+        
+        return hasPathSum(root.left,targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+    }
+    */
+}
