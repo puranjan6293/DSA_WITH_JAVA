ff89e3729c01c772b860da07f3f04935b8886a3c
puranjan <mallik.puranjan@gmail.com>
2022-10-27 18:30:05 +0530

    invert tree

---
+++ src/Leetcode/Invert_BinaryTree.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+public class Invert_BinaryTree {
+    /*
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null) return null;
+        
+        TreeNode temp = root.left;
+        //swaping
+        root.left = root.right;
+        root.right = temp;
+        
+        //calling recursion for left and right
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+     */
+}
