ede3ef536dccfa10cf38b17bbd8793d3f98cb471
puranjan <mallik.puranjan@gmail.com>
2022-10-29 16:43:28 +0530

    swap nodes in pair

---
+++ src/Leetcode/Swap_Node_In_Pair.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class Swap_Node_In_Pair {
+    /*
+    public ListNode swapPairs(ListNode head) {
+        //Base case
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+        while (current.next != null && current.next.next != null) {
+            
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            //swap
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            //move
+            current = current.next.next;
+        }
+        return dummy.next;
+    }
+     */
+}
