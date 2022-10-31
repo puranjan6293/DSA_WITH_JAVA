26d2d1c6921ab42c42b6643a98a4891a9246e9ca
puranjan <mallik.puranjan@gmail.com>
2022-10-31 19:57:19 +0530

    zigzag conversio

---
+++ src/Leetcode/ZigZag_Conversion_String.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class ZigZag_Conversion_String {
+    public String convert(String s, int numRows) {
+        char[] c = s.toCharArray();
+        int len = c.length;
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i = 0; i < sb.length; i++) sb[i] = new StringBuffer();
+    
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) // vertically down
+                sb[idx].append(c[i++]);
+            for (int idx = numRows-2; idx >= 1 && i < len; idx--) // obliquely up
+                sb[idx].append(c[i++]);
+        }
+        for (int idx = 1; idx < sb.length; idx++)
+            sb[0].append(sb[idx]);
+        return sb[0].toString();
+    }
+}
