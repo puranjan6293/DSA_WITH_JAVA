50dcccdf3dc6b8972063069fb4aa9b9bdf499e2b
puranjan <mallik.puranjan@gmail.com>
2022-11-01 22:32:23 +0530

    remove duplicates from a sorted linked list

---
+++ src/Leetcode/Remove_Duplicates_from_sorted_LList.java
@@ -0,0 +1,21 @@
+package Leetcode;
+
+public class Remove_Duplicates_from_sorted_LList {
+    /*
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        ListNode curr = head;
+        
+        while(curr.next!=null){
+            if(curr.next.val==curr.val){
+                curr.next = curr.next.next;
+            }
+            else curr = curr.next;
+        }
+        return head;
+    }
+     */
+}
