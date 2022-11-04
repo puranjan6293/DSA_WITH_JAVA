70ba1e0ad0eba3b8e8b1e984eece3ecc687a943e
puranjan <mallik.puranjan@gmail.com>
2022-11-04 18:37:56 +0530

    remove linkedlist elements

---
+++ src/Leetcode/Remove_LinkedList_Elements.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+public class Remove_LinkedList_Elements {
+    /*
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null) return head;
+        ListNode curr = head;
+        while(curr.next!=null){
+            if(curr.next.val==val){
+                curr.next = curr.next.next;
+            }
+            else curr = curr.next;
+        }
+        if(head.val==val) return head.next;
+        return head;
+    }
+     */
+}
