c18977f002305486475592dc1410c6e05d707b0e
puranjan <mallik.puranjan@gmail.com>
2022-11-04 18:16:33 +0530

    largest no

--- src/Leetcode/Largest_Number.java
+++ src/Leetcode/Largest_Number.java
@@ -1,5 +1,33 @@
 package Leetcode;
 import java.util.*;
 public class Largest_Number {
-    
+    public String largestNumber(int[] nums) {
+        //base case
+        if(nums.length==0) return "";
+        
+        //convert int array to string and add in to a string array
+        String[] conv = new String[nums.length];
+        for(int i=0;i<nums.length;i++){
+            conv[i] = Integer.toString(nums[i]);
+        }
+        
+        //compare numerical values of two strings using lambda function and sort
+        // for eeample:[12,5]
+        // a+b=125
+        // b+a=512
+        Arrays.sort(conv,(a,b)->(b+a).compareTo(a+b));
+        
+        //make a new String using StringBuilder
+        StringBuilder ans = new StringBuilder();
+        
+        //after sorting if the array start with 0 then return 0
+        if(conv[0].equals("0")) return "0";
+        
+        //else append all values in ans
+        for(int i=0;i<conv.length;i++){
+            ans.append(conv[i]);
+        }
+        
+        return ans.toString();
+    }
 }
