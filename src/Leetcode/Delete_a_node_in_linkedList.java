721fc808bde698eba33f7183a57c7d4f21440126
puranjan <mallik.puranjan@gmail.com>
2022-11-06 10:38:10 +0530

    delete a node in linked list

---
+++ src/Leetcode/Delete_a_node_in_linkedList.java
@@ -0,0 +1,10 @@
+package Leetcode;
+
+public class Delete_a_node_in_linkedList {
+    /*
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+    */
+}
