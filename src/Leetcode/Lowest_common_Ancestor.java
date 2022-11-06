a155a07d25d8f003bb9680bdc3d1fa614476dc5f
puranjan <mallik.puranjan@gmail.com>
2022-11-06 10:33:48 +0530

    lowest common ansestor

---
+++ src/Leetcode/Lowest_common_Ancestor.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+public class Lowest_common_Ancestor {
+    /*
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //case 1
+        if(root==null) return null;
+        //case2
+        if(root.val==p.val || root.val==q.val) return root;
+        //case3
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        
+        //case4
+        if(left==null) return right;
+        if(right==null) return left;
+        
+        return root;
+        
+    }
+    */
+}
