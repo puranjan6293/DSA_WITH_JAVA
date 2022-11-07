9f2f967e05953764fdb1f386062b3f15614fcf97
puranjan <mallik.puranjan@gmail.com>
2022-11-07 15:06:21 +0530

    valid

--- src/Leetcode/Valid_paranthesis.java
+++ src/Leetcode/Valid_paranthesis.java
@@ -3,19 +3,17 @@ package Leetcode;
 import java.util.Stack;
 
 public class Valid_paranthesis {
-    public class Valid_Parentheses {
-        public boolean isValid(String s) {
-            Stack<Character> st = new Stack<>();
-            for(int i=0;i<s.length();i++){
-                char a = s.charAt(i);
-                if(a=='(' || a=='[' || a=='{') st.push(a);
-                else if(st.empty()) return false;
-                else if(a==')' && st.pop()!='(') return false;
-                else if(a==']' && st.pop()!='[') return false;
-                else if(a=='}' && st.pop()!='{') return false;
-    
-            }
-            return st.empty();
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char a = s.charAt(i);
+            if(a=='(' || a=='[' || a=='{') st.push(a);
+            else if(st.empty()) return false;
+            else if(a==')' && st.pop()!='(') return false;
+            else if(a==']' && st.pop()!='[') return false;
+            else if(a=='}' && st.pop()!='{') return false;
+
         }
+        return st.empty();
     }
 }
