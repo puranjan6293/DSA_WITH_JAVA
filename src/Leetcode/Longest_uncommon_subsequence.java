8509599f5e36e30e01b297ba1bb007e30d871b66
puranjan <mallik.puranjan@gmail.com>
2022-11-18 23:35:57 +0530

    Longest uncommon subsequence

---
+++ src/Leetcode/Longest_uncommon_subsequence.java
@@ -0,0 +1,7 @@
+package Leetcode;
+
+public class Longest_uncommon_subsequence {
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+    }
+}
