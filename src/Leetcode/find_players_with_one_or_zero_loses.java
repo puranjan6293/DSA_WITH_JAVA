4fc31f4406d3907e8d1c6442b69136b142d89421
puranjan <mallik.puranjan@gmail.com>
2022-11-29 20:48:18 +0530

    0 or one loses

---
+++ src/Leetcode/find_players_with_one_or_zero_loses.java
@@ -0,0 +1,38 @@
+package Leetcode;
+import java.util.*;
+public class find_players_with_one_or_zero_loses {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int n=matches.length;
+        for(int i=0;i<n;i++){
+            if(hm.containsKey(matches[i][0]))
+                hm.put(matches[i][0],hm.get(matches[i][0]));
+            else
+                hm.put(matches[i][0],0);
+            
+            if(hm.containsKey(matches[i][1]))
+                hm.put(matches[i][1],hm.get(matches[i][1])+1);
+            else
+                hm.put(matches[i][1],1);
+        }
+
+        List<List<Integer>> list=new ArrayList<>();
+        List<Integer> list1=new ArrayList<>();
+        List<Integer> list2=new ArrayList<>();
+
+        for(int key:hm.keySet()){
+            if(hm.get(key)==0){
+                list1.add(key);
+            }
+            if(hm.get(key)==1){
+                list2.add(key);
+            }
+        }
+
+        list.add(list1);
+        list.add(list2);
+        Collections.sort(list1);
+        Collections.sort(list2);
+        return list;
+    }
+}
