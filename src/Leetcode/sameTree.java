afbc80eba13e4512c2389f0461b6a8d04f119b09
puranjan <mallik.puranjan@gmail.com>
2023-01-04 22:46:44 +0530

    sameTree

---
+++ src/Leetcode/sameTree.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class sameTree {
+    /*
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //Approach, using bit magic, T.C : O(n)
+
+        //case1, if both are null tree
+        if(p==null && q==null) return true;
+        //case2, if one tree is null tree or they have different values
+        if(p==null || q== null || p.val!=q.val) return false;
+
+        //recusion, repeat case2 by using recursion
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    */
+}
